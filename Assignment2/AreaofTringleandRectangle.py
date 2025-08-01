53b78cde3e6e436eb5e6b8969989637857aed5fd
Darshana-wagh <darshanawagh42161@gmail.com>
2025-08-01 11:57:37 -0700

    Add files via upload

---
+++ Assignment2/AreaofTringleandRectangle.py
@@ -0,0 +1,13 @@
+r = int (input("Enter r"))
+
+area = 3.14*r**2
+
+Circumference = 2*3.14*r
+
+print(f'area of circle:{area}')
+print(f'circumference of circle:{Circumference}')
+
+num1 = int(input("Enter length:"))
+num2 = int(input("Enter breath:"))
+area = num1 + num2
+print("Area of rectangle:",area)
---
+++ Assignment2/celsius.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter temperature in celcius:"))
+
+fahrenteit = celsius * (9/5)+ 32
+
+print(f'temperature in fahrenteint:{fahrenteit}')
